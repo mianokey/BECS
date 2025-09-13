e151637e374479bca28a7bd0f6d867f6b78cee67
mianokey <mianokey@gmail.com>
2025-09-13 22:05:44 +0300

    Added migration  web route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
-
-
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "Migrations run!";
+});
 
 
 
